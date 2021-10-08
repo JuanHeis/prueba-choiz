795e8f73c70b15d161bf9ff4bf52ab95b941a7ba
JuanHeis <juaany98@gmail.com>
2021-10-08 14:13:48 -0300

    Fix detalles

--- src/components/general/Modal.tsx
+++ src/components/general/Modal.tsx
@@ -18,58 +18,62 @@ function Modal(props: { open: boolean, closeModal: any }) {
           <span onClick={handleClick}>X</span>
         </div>
         <h3>Paso a paso para la consulta remota</h3>
-        <div className="modal__grid">
-          <div className="modal__grid__img">
-            <img src={one} alt="" />
-            <img className="rect" src={rectangle} alt="" />
+
+        <div className="modal__grid__container">
+          <div className="modal__grid">
+            <div className="modal__grid__img">
+              <img src={one} alt="" />
+              <img className="rect" src={rectangle} alt="" />
+            </div>
+
+
+            <div className="modal__grid__number">.1</div>
+            <p className="modal__grid__p">
+              Completás algunas preguntas breves y nos contás el motivo
+              de la consulta.
+            </p>
+            <span className="modal__grid__span">
+              Vamos a elegir la doctora adecuada según su especialidad y tu necesidad.
+            </span>
+            <span className="span__exception" />
+
+
+
+            <div className="modal__grid__img">
+
+              <img src={two} alt="" />
+              <img className="rect" src={rectangle} alt="" />
+
+            </div>
+            <div className="modal__grid__number">.2</div>
+
+            <p className="modal__grid__p">
+              Te contactamos por y coordinamos el pago para reservar la fecha.
+            </p>
+            <span className="modal__grid__span">
+              Si tenés Swiss Medical es GRATIS* y si no, podes pedir reintegro
+            </span>
+
+            <span className="span__exception">
+              *Excepto para planes que requieren copago
+            </span>
+
+            <div className="modal__grid__img">
+              <img src={three} alt="" />
+            </div>
+            <div className="modal__grid__number">.3</div>
+
+            <p className="modal__grid__p">
+              Tenés la videollamada con tu doctora, desde donde estés y sin límite de tiempo.
+            </p>
+            <span className="modal__grid__span">
+              ¡Dura lo que vos necesites!
+            </span>
+            <span className="span__exception" />
+
           </div>
-
-
-          <div className="modal__grid__number">.1</div>
-          <p className="modal__grid__p">
-            Completás algunas preguntas breves y nos contás el motivo
-            de la consulta.
-          </p>
-          <span className="modal__grid__span">
-            Vamos a elegir la doctora adecuada según su especialidad y tu necesidad.
-          </span>
-          <span className="span__exception" />
-
-
-
-          <div className="modal__grid__img">
-
-            <img src={two} alt="" />
-            <img className="rect" src={rectangle} alt="" />
-
-          </div>
-          <div className="modal__grid__number">.2</div>
-
-          <p className="modal__grid__p">
-            Te contactamos por y coordinamos el pago para reservar la fecha.
-          </p>
-          <span className="modal__grid__span">
-            Si tenés Swiss Medical es GRATIS* y si no, podes pedir reintegro
-          </span>
-
-          <span className="span__exception">
-            *Excepto para planes que requieren copago
-          </span>
-
-          <div className="modal__grid__img">
-            <img src={three} alt="" />
-          </div>
-          <div className="modal__grid__number">.3</div>
-
-          <p className="modal__grid__p">
-            Tenés la videollamada con tu doctora, desde donde estés y sin límite de tiempo.
-          </p>
-          <span className="modal__grid__span">
-            ¡Dura lo que vos necesites!
-          </span>
-          <span className="span__exception" />
-
         </div>
+
       </div>
     </div>
   )
